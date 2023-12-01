fa7f905444744fa85dd66c6bd7c3e82478dd6877
luis correa <ylss512c@gmail.com>
2023-11-30 21:03:10 -0300

    limpiando proyecto y añadiendo react routers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {/* en Routes van las rutas a mostrarse // Afuera de Routes elementos estaticos como navbar o footer */}
+      <Routes>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
