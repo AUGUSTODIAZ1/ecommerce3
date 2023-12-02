2ea89efda7e2559bcc4a12d8474c7183794d8402
AleGalarze21 <alegalarzeoficial@gmail.com>
2023-12-02 17:44:35 -0300

    Union-Navbar

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FooterAD } from './components/footerPage/FooterAD';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComp from './components/footerPage/Navbar-Component/NavbarComp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       {/* Afuera de Routes elementos estaticos como navbar o footer */}
+        <NavbarComp path='/Navbar' elemt={<NavbarComp />} />
       <Routes>
         {/* En Routes van las rutas a mostrarse  */}
       </Routes>
