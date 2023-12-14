c16124878f1616c9c41d79886ad27d44443337a9
AleGalarze21 <alegalarzeoficial@gmail.com>
2023-12-14 10:58:30 -0300

    Navbar-Cambios

---
+++ src/Components/footerPage/Navbar-Component/DropDownProfile.jsx
@@ -0,0 +1,15 @@
+// import React from 'react'
+
+// const DropDownProfile = () => {
+//   return (
+//     <div className='flex flex-col dropDownProfile'>
+//         <ul className='flex flex-col gap-4'>
+//             <li>Perfil</li>
+//             <li>Configuración</li>
+//             <li>Cerrar Sesión</li>
+//         </ul>
+//     </div>
+//   );
+// };
+
+// export default DropDownProfile;
--- src/Components/footerPage/Navbar-Component/NavbarComp.modules.jsx
+++ src/Components/footerPage/Navbar-Component/NavbarComp.modules.jsx
@@ -1,12 +1,23 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import '../Navbar-Component/Nav.modules.css'
+import '../Navbar-Component/Nav.modules.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleInfo, faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+// import DropDownProfile from './DropDownProfile';
+// import { useState } from 'react';
+// import { useSelector } from 'react-redux';
 
-const NavbarComp = () => {
-  return (
-    <Navbar expand="lg" className="Navbar bg-tertiary" data-bs-theme="dark">
+    
+    const NavbarComp = () => {
+    //   const [open, setOpen] = useState(false)
+    //   const [register, setRegister] = useState(false)
+
+    //   const storeName = useSelector(state => state.user.nome)
+    
+    return (
+      <Navbar expand="lg" className="Navbar bg-tertiary" data-bs-theme="dark">
       <Container className='opacity'>
         <img className="img-Logo" src="https://trello.com/1/cards/656b68d038e71faa56cd0e8b/attachments/656b68fc26b01dc1723afe97/previews/656b68fd26b01dc1723afed6/download/klipartz.com.png" alt="imgLogo" href="#" ></img>
-        <Navbar.Brand href="#home" className='color-stor'>Store Apple</Navbar.Brand>
+        <Navbar.Brand href="/" className='color-stor'>Store Apple</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" className='color-toggle' />
         <Navbar.Collapse className='opacity'>
           <Nav className="t-1 ms-auto">
@@ -14,18 +25,30 @@ const NavbarComp = () => {
             <Nav.Link className='colorSS' href="#favoritos">Favoritos</Nav.Link>
             <Nav.Link className='colorSS' href="#contacto">Contacto</Nav.Link>
           </Nav>
+
+          <Nav className='buscar'>
+            <input type="text" placeholder='Buscar' required />
+            <FontAwesomeIcon className='btn-1' data-bs-toggle='modal' icon={faMagnifyingGlass}/>
+          </Nav>
+
           <Nav className='ms-auto'>
-            <Nav.Link className='btn  colorSS' href="#contacto">Inciar Sessión</Nav.Link>
+            <Nav.Link className='btn  colorSS' href="#contacto">Iniciar Sesión</Nav.Link>
             <Nav.Link className='btn  colorSS' href="#contacto">Registrarse</Nav.Link>
           </Nav>
-          <Nav className=''>
 
-          </Nav>
+          <a href="/Error">
+            <FontAwesomeIcon className='info-2' icon={faCircleInfo}/>
+          </a>
+          <a href="#">
+            <FontAwesomeIcon className='info-2' icon={faCartShopping}/>
+          </a>
           
+          {/* <DropDownProfile /> */}
+
         </Navbar.Collapse>
       </Container>
     </Navbar>
-  )
-}
+  );
+};
 
 export default NavbarComp;
